7e570eb5608736f3e0daba95f4e6528059561601
Carter Ithier <carter.ithier@gmail.com>
2020-04-15 16:12:10 -0700

    added pose and perspective transform to tracking.py Feature matching is still messed up giving poor results for most images

--- src/original/homography_and_pose.py
+++ src/original/homography_and_pose.py
@@ -2,115 +2,133 @@ import cv2 as cv
 import numpy as np
 import math
 
-"""
-Inputs: H - a 3x3 homography matrix
-        K - the intrinsic camera matrix found in the calibration step
-Outputs: T - the 3 x 4 transformation matrix (it has both rotation and translation)
-         R - the 3 x 3 rotation matrix 
-         t - the 3 x 1 translation vector
-Description: Get the rotation matrix, translation vector, and combined transformation matrix
-from a homography matrix and camera parameters
-"""
-def get_rotation_matrix(H, K):
-    H = np.matmul(np.linalg.pinv(K), H)
+class HomographyandPose:
+    """
+    Inputs: H - a 3x3 homography matrix
+            K - the intrinsic camera matrix found in the calibration step
+    Outputs: T - the 3 x 4 transformation matrix (it has both rotation and translation)
+             R - the 3 x 3 rotation matrix
+             t - the 3 x 1 translation vector
+    Description: Get the rotation matrix, translation vector, and combined transformation matrix
+    from a homography matrix and camera parameters
+    """
+    @staticmethod
+    def get_rotation_matrix(H, K):
+        H = np.matmul(np.linalg.pinv(K), H)
 
-    norm = math.sqrt(H[0,0]**2 + H[1,0]**2 + H[2,0]**2)
+        norm = math.sqrt(H[0,0]**2 + H[1,0]**2 + H[2,0]**2)
 
-    H = H / norm
+        H = H / norm
 
-    rc1 = H[:, 0].reshape((3,1))
-    rc2 = H[:, 1].reshape((3,1))
-    rc3 = np.cross(rc1, rc2, axis=0)
-    t = H[:, 2].reshape((3,1))
+        rc1 = H[:, 0].reshape((3,1))
+        rc2 = H[:, 1].reshape((3,1))
+        rc3 = np.cross(rc1, rc2, axis=0)
+        t = H[:, 2].reshape((3,1))
 
-    R = np.hstack((rc1, rc2, rc3))
-    T = np.hstack((R, t))
+        R = np.hstack((rc1, rc2, rc3))
+        T = np.hstack((R, t))
 
-    return T, R, t
+        return T, R, t
 
-"""
-Inputs: T - the transformation matrix in form [R | t] where R is a rotation matrix and t
-        is the translation vector. It is of size (3, 4)
-        K - the intrinsic camera calibration matrix determined in the camera calibration step
-        pts - the pts that you want projected from 3D space into 2D space
-Outputs: a numpy array where all input pts have been projected onto the 2D plane of the image
-"""
-def project_pts(T, K, pts):
-    projected = []
-    for pt in pts:
-        pt = np.append(pt, 1)
-        p1 = np.matmul(K, T)
-        p = np.matmul(p1, pt)
-        p = p / p[-1]
-        p = p.astype(int)
-        projected.append(p)
-    return np.array(projected)
+    """
+    Inputs: T - the transformation matrix in form [R | t] where R is a rotation matrix and t
+            is the translation vector. It is of size (3, 4)
+            K - the intrinsic camera calibration matrix determined in the camera calibration step
+            pts - the pts that you want projected from 3D space into 2D space
+    Outputs: a numpy array where all input pts have been projected onto the 2D plane of the image
+    """
+    @staticmethod
+    def project_pts(T, K, pts):
+        projected = []
+        for pt in pts:
+            pt = np.append(pt, 1)
+            p1 = np.matmul(K, T)
+            p = np.matmul(p1, pt)
+            p = p / p[-1]
+            p = p.astype(int)
+            projected.append(p)
+        return np.array(projected)
 
-"""
-Inputs: img - the img you want to draw lines on
-        corners - a list of corners where the 0th element is the pixel location for the origin
-            of the axis
-        pts - a (3,3) numpy array that has three points that when connected to the origin
-            create the x, y, z axis
-Outputs: an image with x, y, z axis in red, green, blue drawn on it
-Description: Given an origin (corners[0]) and three points that have been projected from 3D space
-to the 2D plane, draw x, y, z axis
-"""
-def draw(img, corners, pts):
-    pts = pts.flatten().reshape((3, 3))
-    corners = corners.flatten().reshape((42, 2))
-    corner = tuple(corners[0])
-    img = cv.line(img, corner, tuple(pts[0, 0:2]), (255, 0, 0), 5)
-    img = cv.line(img, corner, tuple(pts[1, 0:2]), (0, 255, 0), 5)
-    img = cv.line(img, corner, tuple(pts[2, 0:2]), (0, 0, 255), 5)
-    return img
+    """
+    Inputs: img - the img you want to draw lines on
+            corners - a list of corners where the 0th element is the pixel location for the origin
+                of the axis
+            pts - a (3,3) numpy array that has three points that when connected to the origin
+                create the x, y, z axis
+    Outputs: an image with x, y, z axis in red, green, blue drawn on it
+    Description: Given an origin (corners[0]) and three points that have been projected from 3D space
+    to the 2D plane, draw x, y, z axis
+    """
+    @staticmethod
+    def draw(img, corners, pts):
+        pts = pts.flatten().reshape((3, 3))
+        # corners = corners.flatten().reshape((42, 2))
+        corner = tuple(corners[0])
+        img = cv.line(img, corner, tuple(pts[0, 0:2]), (255, 0, 0), 3)
+        img = cv.line(img, corner, tuple(pts[1, 0:2]), (0, 255, 0), 3)
+        img = cv.line(img, corner, tuple(pts[2, 0:2]), (0, 0, 255), 3)
+        return img
 
-"""
-Inputs: objpts - a (n, 2) numpy array of points in the reference frame of the object
-        imgpts - a (n, 2) numpy array of points in the reference frame of the image (
-                the pixel locations of the features)
-Returns: a 3 x 3 homography matrix to get from objpts to imgpts
-Description: calculate a homography matrix using 4 or more coplanar points
-"""
-def find_homography(objpts, imgpts):
-    obj_xy = objpts[0]
-    img_uv = imgpts[0]
-    A, b = make_subset(obj_xy, img_uv)
-    for i in range(1, len(objpts)):
-        obj_xy = objpts[i]
-        img_uv = imgpts[i]
-        sub_A, sub_b = make_subset(obj_xy, img_uv)
-        A = np.vstack((A, sub_A))
-        b = np.vstack((b, sub_b))
+    @staticmethod
+    def perspective_transform(H, pts):
+        transformed_pts = []
+        for pt in pts:
+            pt = np.append(pt, 1).reshape((3, 1))
+            p = np.matmul(H, pt)
+            transformed_pts.append([p[0] / p[2], p[1] / p[2]])
+        return transformed_pts
 
-    x = np.matmul(np.linalg.pinv(A), b)
-    h33 = np.array([[1]])
+    """
+        Inputs: xy - a (1,2) numpy array of a point in the reference frame of the object
+                uv - a (1,2) numpy array of a point in the reference frame of the image (the pixel 
+                    location of the feature)
+        Returns: array - a (2, 8) numpy array that is part of the array for calculations for the homography
+                    matrix
+                b = a (2, 1) numpy array containing the point in the reference frame of the image
+        Description: compute subarrays of the two matrices used to calculate the homography matrix in the
+        find_homography function
+        """
 
-    x = np.vstack((x, h33))
+    @staticmethod
+    def make_subset(xy, uv):
+        x = xy[0]
+        y = xy[1]
+        u = uv[0]
+        v = uv[1]
+        array = np.array([[x, y, 1, 0, 0, 0, -u * x, - u * y],
+                          [0, 0, 0, x, y, 1, -v * x, -v * y]]).reshape((2, 8))
+        b = np.array([[u, v]]).reshape((2, 1))
+        return array, b
 
-    H = x.reshape((3, 3))
+    """
+    Inputs: objpts - a (n, 2) numpy array of points in the reference frame of the object
+            imgpts - a (n, 2) numpy array of points in the reference frame of the image (
+                    the pixel locations of the features)
+    Returns: a 3 x 3 homography matrix to get from objpts to imgpts
+    Description: calculate a homography matrix using 4 or more coplanar points
+    """
+    @staticmethod
+    def find_homography(objpts, imgpts):
+        obj_xy = objpts[0]
+        img_uv = imgpts[0]
+        A, b = HomographyandPose.make_subset(obj_xy, img_uv)
+        for i in range(1, len(objpts)):
+            obj_xy = objpts[i]
+            img_uv = imgpts[i]
+            sub_A, sub_b = HomographyandPose.make_subset(obj_xy, img_uv)
+            A = np.vstack((A, sub_A))
+            b = np.vstack((b, sub_b))
 
-    return H
+        x = np.matmul(np.linalg.pinv(A), b)
+        h33 = np.array([[1]])
 
-"""
-Inputs: xy - a (1,2) numpy array of a point in the reference frame of the object
-        uv - a (1,2) numpy array of a point in the reference frame of the image (the pixel 
-            location of the feature)
-Returns: array - a (2, 8) numpy array that is part of the array for calculations for the homography
-            matrix
-        b = a (2, 1) numpy array containing the point in the reference frame of the image
-Description: compute subarrays of the two matrices used to calculate the homography matrix in the
-find_homography function
-"""
-def make_subset(xy, uv):
-    x = xy[0]
-    y = xy[1]
-    u = uv[0]
-    v = uv[1]
-    array = np.array([[x, y, 1, 0, 0, 0, -u * x, - u * y],
-                      [0, 0, 0, x, y, 1, -v * x, -v * y]]).reshape((2, 8))
-    b = np.array([[u, v]]).reshape((2, 1))
-    return array, b
+        x = np.vstack((x, h33))
+
+        H = x.reshape((3, 3))
+
+        return H
+
+
 
 def draw_cv(img, corners, imgpts):
     corner = tuple(corners[0].ravel())
