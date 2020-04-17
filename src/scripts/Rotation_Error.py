e94c6905581fb048f032b9ca8c67de22903a4290
Carter Ithier <carter.ithier@gmail.com>
2020-04-17 12:16:56 -0700

    fixed rotation error script to get the callback correctly. Still need to update position error with custom message ttype

--- src/scripts/Rotation_Error.py
+++ src/scripts/Rotation_Error.py
@@ -5,7 +5,7 @@ import signal
 import math
 import rospy
 from std_msgs.msg import Float64
-from ar_track_alvar_msgs.msg import AlvarMarker
+from ar_track_alvar_msgs.msg import AlvarMarkers, AlvarMarker
 from geometry_msgs.msg import Pose 
 
 class Rotation_Error:
@@ -19,24 +19,29 @@ class Rotation_Error:
         self.y2 = 0
         self.z2 = 0
         self.w2 = 0
-	print("hi")
-	print(self.x1)
         self.data = Float64()
         self.error_Publisher = rospy.Publisher('/error', Float64, queue_size=1)
 
     # callback for the pose received from the ar tag
     def ar_tag_callback(self, msg):
-	print("ar")
-        self.x1 = msg.pose.pose.orientation.x
-        self.y1 = msg.pose.pose.orientation.y
-        self.z1 = msg.pose.pose.orientation.z
-        self.w1 = msg.pose.pose.orientation.w
-        q = self.x1*self.x2 + self.y1*self.y2 + self.z1*self.z2 + self.w1*self.w2
-	print(q)
-        self.data = math.acos(2*(q**2) - 1)
+        print("ar")
+        markers = msg.markers
+        if len(markers) > 0:
+            msg = markers[0]
+            self.x1 = msg.pose.pose.orientation.x
+            self.y1 = msg.pose.pose.orientation.y
+            self.z1 = msg.pose.pose.orientation.z
+            self.w1 = msg.pose.pose.orientation.w
+            q = self.x1*self.x2 + self.y1*self.y2 + self.z1*self.z2 + self.w1*self.w2
+            print("q", q)
+            self.data = math.acos(2*(q**2) - 1)
+            print("pose error", self.data)
+        else:
+            print("no marker found")
 
     # callback for the pose received from the object tracker 
     def tracking_callback(self, msg):
+        print("our tracking")
         self.x2 = msg.pose.orientation.x
         self.y2 = msg.pose.orientation.y
         self.z2 = msg.pose.orientation.z
@@ -52,8 +57,8 @@ def main():
     rospy.init_node("error")
     print("main")
     rotation_error = Rotation_Error()
-    rospy.Subscriber('/ar_pose_marker',AlvarMarker,rotation_error.ar_tag_callback)
-    rospy.Subscriber('/topic_from_our_method',Pose,rotation_error.tracking_callback)
+    rospy.Subscriber('/ar_pose_marker', AlvarMarkers, rotation_error.ar_tag_callback)
+    rospy.Subscriber('/topic_from_our_method', Pose, rotation_error.tracking_callback)
     rate = rospy.Rate(30)
 
     while not rospy.is_shutdown():
